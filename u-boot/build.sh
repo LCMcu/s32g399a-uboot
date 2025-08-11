a67347cc0f64976461434810f80f698f26d0e9da
LCMcu <1771621931@qq.com>
2025-08-10 23:07:46 -0700

    修改环境变量hwconfig为serdes0:mode=pcie&xpcs1,clock=ext,fmhz=100;pcie0:mode=rc;xpcs0_1:speed=1G,an=0;serdes1:mode=xpcs0&xpcs1,clock=ext,fmhz=125;xpcs1_0:speed=1G;xpcs1_1:speed=1G

--- u-boot/build.sh
+++ u-boot/build.sh
@@ -1,3 +1,8 @@
-make CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- s32g399ardb3_qspi_defconfig
+#make CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- s32g399ardb3_qspi_defconfig
 
-make CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+if [ $# -eq 0 ]; then
+    echo "错误：请提供一个参数"
+    exit 1
+fi
+
+make CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- $1 
