374ce0cdfb95d18195dd81f8f002c8c728c92f26
LCMcu <1771621931@qq.com>
2025-08-10 22:59:58 -0700

    基于S32G399A BSP43

---
+++ u-boot/build.sh
@@ -0,0 +1,3 @@
+make CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- s32g399ardb3_qspi_defconfig
+
+make CROSS_COMPILE=/home/lc/work/tools/arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
